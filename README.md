6989e48848bd017a8b2973457a03e6973b8eef08
Nitin <nitinharane13@gmail.com>
2024-02-26 14:30:45 +0530

    File Processor

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# crest_test
+# File Processor
+The goal for this task is to design and develop read data from the multiple file which are located in particular folder.
+## Required Libraries
+pandas 
+## Installation
+'''pip install pandas'''
---
+++ file_processor.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from csv import writer
+import os
+
+input_filepath = './data/'
+output_filepath = './output/'
+
+
+def process_data(data_files):
+    """This function is to read and process data files"""
+    try:
+        frames = []
+        output_file = output_filepath + 'result.csv'
+        for file in data_files:
+            df = pd.read_csv(input_filepath + file, delimiter='\t')
+            df.drop_duplicates()  # remove duplicates from dataframe
+            frames.append(df)
+        result = pd.concat(frames, ignore_index=True)
+        result['gross_salary'] = result[['basic_salary', 'allowances']].sum(axis=1)
+        result.to_csv(output_file, index=False)
+        high_salary = 'Second Highest Salary:{}'.format(result['gross_salary'].nlargest(2))
+        avg_salary = 'Average Salary:{}'.format(result['gross_salary'].mean())
+        summary = [high_salary, avg_salary]
+        with open(output_file, 'a', newline='') as f_object:
+            writer_object = writer(f_object)
+            writer_object.writerow(summary)
+            f_object.close()
+        status = "Files has been processed Successfully"
+        return status
+    except Exception as e:
+        status = "Error while processing file: {}".format(e)
+        return status
+
+
+file_list = [x for x in os.listdir(input_filepath) if x.endswith('.dat')]
+print(process_data(file_list))
